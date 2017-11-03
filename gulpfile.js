1f059a27764d9ed9c23f98cf41651d5984aa1cda
JamesBaine <jamesmbaine@gmail.com>
2017-11-03 12:05:09 -0700

    gulp scss compiler, min.css linked to app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // dependencies
 
@@ -6,6 +6,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minifyCSS = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var changed = require("gulp-changed");
 
 
@@ -14,3 +15,25 @@ var changed = require("gulp-changed");
 /////////////////
 // - SCSS/css
 /////////////////
+
+var SCSS_SRC = "./src/assets/scss/**/*.scss";
+var SCSS_DEST ="./src/assets/css";
+
+// Compile SCSS
+gulp.task("compile_scss", function(){
+	gulp.src(SCSS_SRC)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(minifyCSS())
+		.pipe(rename({suffix: ".min" }))
+		.pipe(changed(SCSS_DEST))
+		.pipe(gulp.dest(SCSS_DEST));
+});
+
+
+// detect changes in SCSS
+gulp.task("watch_scss",function() {
+	gulp.watch(SCSS_SRC,["compile_scss"]);
+});
+
+// run tasks
+gulp.task("default", ["watch_scss"]);
