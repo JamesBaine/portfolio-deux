1c834fcdaac06232e9058a611c6d212cea9368aa
JamesBaine <jamesmbaine@gmail.com>
2017-11-05 11:20:36 -0800

    basic react components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+
+// components
+import Header from "./components/headerComponent/header";
+import Footer from "./components/footerComponent/footer";
+import HomePage from "./components/pages/homePage";
+
+// includes
 import "./assets/css/default.min.css";
+
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-			Test
+
+				<Header />
+
+				<HomePage />
+
+				<Footer />
+
 			</div>
 		);
 	}
