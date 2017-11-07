e3f4067e549871b45599557f67ca9a580f270b07
JamesBaine <jamesmbaine@gmail.com>
2017-11-06 17:55:09 -0800

    basic styling

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 class HomePage extends Component {
 	render() {
 		return (
-	    <div className="wrap">
-        Home Page Content Here
+			<div className="container-fluid">
+
+				<h1>Home Page Content</h1>
+
+				<p>Up and coming web developer with a passion for learning with an endless supply of optimism and curiosity.</p>
+				<p>One of my stronger points is writing front-ends that are smooth, simple and aesthetically pleasing. I primary use languages and libraries such as Javascript, CSS and Sass but I'm always learning new ones to try to find more efficient ways of doing things.</p>
+				<p>I am also growing increasingly comfortable and familiar with back-end development using Node.js, MySQL, Sequelize, etc.</p>
+				<p>I'm based in LA right now but I love traveling and going to new places so I am open to relocating if need be.</p>
+
+
 			</div>
 		);
 	}
