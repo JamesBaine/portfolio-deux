4b884df5cf45c6002d61e2b6c1c4d152b04c0445
JamesBaine <jamesmbaine@gmail.com>
2017-11-12 22:22:39 -0800

    link changed to overlay

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -7,14 +7,14 @@ class Project extends Component {
 	render() {
 		return (
 			<div className="project" style= {{backgroundImage: "url(" + this.props.image + ")"}}>
-				<div className="overlay">
+				<a  href={this.props.site} className="overlay">
 					<p className="description">
 						{this.props.children}
 					</p>
-					<a className="title" href={this.props.site}>
+					<h2 className="title">
 						{this.props.title}
-					</a>
-				</div>
+					</h2>
+				</a>
 			</div>
 		);
 	}
