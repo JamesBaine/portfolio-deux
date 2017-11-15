e0159a1c3342c2a4b16f286e835fd6d099c7e61d
JamesBaine <jamesmbaine@gmail.com>
2017-11-14 22:30:12 -0800

    footer, icon links added

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -5,7 +5,22 @@ class Footer extends Component {
 		return (
 			<footer>
 
-				Footer
+				<p className="connect">Connect with me</p>
+
+				<ul>
+					<li className="first">
+						<a href="mailto:jamesmbaine@gmail.com"><img className="email-icon" src={require("../../assets/photos/gmail-grey.png")} alt=""/></a>
+					</li>
+					<li>
+						<a href="mailto:jamesmbaine@gmail.com"><img className="github-icon" src={require("../../assets/photos/git-grey.png")} alt="github"/></a>
+					</li>
+					<li>
+						<a href="mailto:jamesmbaine@gmail.com"><img className="linkedin-icon" src={require("../../assets/photos/link-grey.png")} alt="linkedin"/></a>
+					</li>
+					<li>
+						<a href="mailto:jamesmbaine@gmail.com"><img className="instagram-icon" src={require("../../assets/photos/insta-grey.png")} alt="instagram"/></a>
+					</li>
+				</ul>
 
 			</footer>
 		);
--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -15,24 +15,28 @@ class Portfolio extends Component {
 
 					<h1>Projects</h1>
 
-					<Project title={Work[0].title} site={Work[0].site} image={Work[0].image}>
-						{Work[0].description}
-					</Project>
-					<Project title={Work[1].title} site={Work[1].site} image={Work[1].image}>
-						{Work[1].description}
-					</Project>
-					<Project title={Work[2].title} site={Work[2].site} image={Work[2].image}>
-						{Work[2].description}
-					</Project>
-					<Project title={Work[3].title} site={Work[3].site} image={Work[3].image}>
-						{Work[3].description}
-					</Project>
-					<Project title={Work[4].title} site={Work[4].site} image={Work[4].image}>
-						{Work[4].description}
-					</Project>
-					<Project title={Work[5].title} site={Work[5].site} image={Work[5].image}>
-						{Work[5].description}
-					</Project>
+					<div className="projects-container">
+
+						<Project title={Work[0].title} site={Work[0].site} image={Work[0].image}>
+							{Work[0].description}
+						</Project>
+						<Project title={Work[1].title} site={Work[1].site} image={Work[1].image}>
+							{Work[1].description}
+						</Project>
+						<Project title={Work[2].title} site={Work[2].site} image={Work[2].image}>
+							{Work[2].description}
+						</Project>
+						<Project title={Work[3].title} site={Work[3].site} image={Work[3].image}>
+							{Work[3].description}
+						</Project>
+						<Project title={Work[4].title} site={Work[4].site} image={Work[4].image}>
+							{Work[4].description}
+						</Project>
+						<Project title={Work[5].title} site={Work[5].site} image={Work[5].image}>
+							{Work[5].description}
+						</Project>
+
+					</div>
 
 					<Skills />
 
